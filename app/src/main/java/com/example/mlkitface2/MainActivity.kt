0bfbe9d8e7433849a00dd0f072cab19279510f6b
user <rG734jQyup2A@outlook.jp>
2021-02-12 18:27:04 +0900

    18

--- app/src/main/java/com/example/mlkitface2/MainActivity.kt
+++ app/src/main/java/com/example/mlkitface2/MainActivity.kt
@@ -1,75 +1,110 @@
 package com.example.mlkitface2
 
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.example.mlkitface2.camerax.CameraManager
+import android.util.Size
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var cameraManager: CameraManager
+    private val executor = Executors.newSingleThreadExecutor()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        createCameraManager()
-        checkForPermission()
-        onClicks()
+
+        startCamera()
     }
 
-    private fun checkForPermission() {
-        if (allPermissionsGranted()) {
-            cameraManager.startCamera()
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                REQUIRED_PERMISSIONS,
-                REQUEST_CODE_PERMISSIONS
-            )
-        }
-    }
+    var highAccuracyOpts = FirebaseVisionFaceDetectorOptions.Builder()
+        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+        .build()
 
-    private fun onClicks() {
-        btnSwitch.setOnClickListener {
-            cameraManager.changeCameraSelector()
-        }
-    }
+    val faceDetector =
+        FirebaseVision.getInstance()
+            .getVisionFaceDetector(highAccuracyOpts)
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults:
-        IntArray
-    ) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                cameraManager.startCamera()
-            } else {
-                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT)
-                    .show()
-                finish()
+    //Function that creates and displays the camera preview
+    private fun startCamera() {
+
+        val previewConfig = PreviewConfig.Builder()
+            .apply {
+                setTargetResolution(Size(1920, 1080))
             }
+            .build()
+
+        val preview = Preview(previewConfig)
+
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = cameraView.parent as ViewGroup
+            parent.removeView(cameraView)
+            parent.addView(cameraView, 0)
+            cameraView.surfaceTexture = it.surfaceTexture
+        }
+
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+
+            setImageReaderMode(
+                ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE
+            )
+        }.build()
+
+        val imageAnalysis = ImageAnalysis(analyzerConfig).apply {
+            analyzer = ImageProcessor()
+        }
+
+        CameraX.bindToLifecycle(this, preview, imageAnalysis)
+    }
+
+    inner class ImageProcessor : ImageAnalysis.Analyzer {
+        private val TAG = javaClass.simpleName
+
+        private var lastAnalyzedTimestamp = 0L
+
+        private fun degreesToFirebaseRotation(degrees: Int): Int = when (degrees) {
+            0 -> FirebaseVisionImageMetadata.ROTATION_0
+            90 -> FirebaseVisionImageMetadata.ROTATION_90
+            180 -> FirebaseVisionImageMetadata.ROTATION_180
+            270 -> FirebaseVisionImageMetadata.ROTATION_270
+            else -> throw Exception("Rotation must be 0, 90, 180, or 270.")
+        }
+
+        override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
+            val currentTimestamp = System.currentTimeMillis()
+            if (currentTimestamp - lastAnalyzedTimestamp >=
+                TimeUnit.SECONDS.toMillis(1)
+            ) {
+                val imageRotation = degreesToFirebaseRotation(rotationDegrees)
+                image?.image?.let {
+                    val visionImage = FirebaseVisionImage.fromMediaImage(it, imageRotation)
+                    faceDetector.detectInImage(visionImage)
+                        .addOnSuccessListener { faces ->
+                            faces.forEach { face ->
+                                if (face.leftEyeOpenProbability < 0.4 || face.rightEyeOpenProbability < 0.4) {
+                                    label.text = "Blinking"
+                                } else {
+                                    label.text = "Not Blinking"
+                                }
+                            }
+                        }
+                        .addOnFailureListener {
+                            it.printStackTrace()
+                        }
+                }
+            }
+
         }
     }
-
-    private fun createCameraManager() {
-        cameraManager = CameraManager(
-            this,
-            previewView_finder,
-            this,
-            graphicOverlay_finder
-        )
-    }
-
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    companion object {
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
-    }
-
 }
