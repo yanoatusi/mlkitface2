fd77543f14afdacdada2df8ffccbfe9146c2ba44
user <rG734jQyup2A@outlook.jp>
2021-02-14 10:54:42 +0900

    19

--- app/src/main/java/com/example/mlkitface2/MainActivity.kt
+++ app/src/main/java/com/example/mlkitface2/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.mlkitface2
 
 
+import android.media.AudioAttributes
+import android.media.SoundPool
 import android.os.Bundle
+import android.util.Log
 import android.util.Size
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
@@ -19,15 +22,43 @@ class MainActivity : AppCompatActivity() {
 
     private val executor = Executors.newSingleThreadExecutor()
 
+    private lateinit var soundPool: SoundPool
+    private var soundOne = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val audioAttributes = AudioAttributes.Builder()
+            // USAGE_MEDIA
+            // USAGE_GAME
+            .setUsage(AudioAttributes.USAGE_GAME)
+            // CONTENT_TYPE_MUSIC
+            // CONTENT_TYPE_SPEECH, etc.
+            .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+            .build()
+
+        soundPool = SoundPool.Builder()
+            .setAudioAttributes(audioAttributes)
+            // ストリーム数に応じて
+            .setMaxStreams(1)
+            .build()
+
+        // one.wav をロードしておく
+        soundOne = soundPool.load(this, R.raw.one, 1)
+
+
+        // load が終わったか確認する場合
+        soundPool.setOnLoadCompleteListener{ soundPool, sampleId, status ->
+            Log.d("debug", "sampleId=$sampleId")
+            Log.d("debug", "status=$status")
+        }
 
         startCamera()
     }
 
     var highAccuracyOpts = FirebaseVisionFaceDetectorOptions.Builder()
-        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.FAST)
         .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
         .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
         .build()
@@ -94,6 +125,9 @@ class MainActivity : AppCompatActivity() {
                             faces.forEach { face ->
                                 if (face.leftEyeOpenProbability < 0.4 || face.rightEyeOpenProbability < 0.4) {
                                     label.text = "Blinking"
+                                    // one.wav の再生
+                                    // play(ロードしたID, 左音量, 右音量, 優先度, ループ, 再生速度)
+                                    soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1.0f)
                                 } else {
                                     label.text = "Not Blinking"
                                 }
